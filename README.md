14340094ff25c180540f225a8ed71c247ed11956
Brian Bailey <brbailey@google.com>
2016-11-09 18:36:52 -0800

    local drawing with playback

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# bike-canvas
+# Bike Canvas
+This project and its [paper](https://docs.google.com/document/d/11AXoIEOM2QBKDXbr_oeL1rdR6JgghLhevXqeBKCfE48/edit?usp=sharing) describe the well-researched psychological phenomenon of overlooking cotidian objects. Then we will explore the consequences of such oversight. Finally, we will consider the outcomes of an experiment in which we asked 10,000 randomly self-selected people to draw a bicycle without looking at a picture.
+
+### References
+* [De/Serializing Recordings in Recordable HTML5 Canvas](http://ramkulkarni.com/blog/deserializing-recordings-in-recordable-html5-canvas/)
+* [Record and Playback Drawing in HTML5 Canvas](http://ramkulkarni.com/blog/record-and-playback-drawing-in-html5-canvas/)
+* [Record and Playback Drawing in HTML5 Canvas – Part II](http://ramkulkarni.com/blog/record-and-playback-drawing-in-html5-canvas-part-ii/)
---
+++ js/redraw.js
@@ -0,0 +1,111 @@
+playbackInterruptCommand = "";
+
+	$(document).bind("pageinit", function()
+	{
+		$("#pauseBtn").hide();
+		$("#playBtn").hide();
+		drawing = new RecordableDrawing("canvas1");
+
+		$("#recordBtn").click(function(){
+			var btnTxt = $("#recordBtn .ui-btn-text").text();
+			if (btnTxt == 'Stop')
+				stopRecording();
+			else
+				startRecording();
+		});
+
+		$("#playBtn").click(function(){
+			var btnTxt = $("#playBtn .ui-btn-text").text();
+			if (btnTxt == 'Stop')
+				stopPlayback();
+			else
+				startPlayback();
+		});
+
+		$("#pauseBtn").click(function(){
+			var btnTxt = $("#pauseBtn .ui-btn-text").text();
+			if (btnTxt == 'Pause')
+			{
+				pausePlayback();
+			} else if (btnTxt == 'Resume')
+			{
+				resumePlayback();
+			}
+		});
+
+		$("#clearBtn").click(function(){
+			drawing.clearCanvas();
+		});
+	});
+
+	function stopRecording()
+	{
+		$("#recordBtn .ui-btn-text").text("Record");
+		$("#playBtn").show();
+		$("#clearBtn").show();
+		$("#pauseBtn").hide();
+
+		drawing.stopRecording();
+	}
+
+	function startRecording()
+	{
+		$("#recordBtn .ui-btn-text").text("Stop");
+		$("#playBtn").hide();
+		$("#pauseBtn").hide();
+		$("#clearBtn").hide();
+
+		drawing.startRecording();
+	}
+
+	function stopPlayback()
+	{
+		playbackInterruptCommand = "stop";
+	}
+
+	function startPlayback()
+	{
+		drawing.playRecording(function() {
+			//on playback start
+			$("#playBtn .ui-btn-text").text("Stop");
+			$("#recordBtn").hide();
+			$("#pauseBtn").show();
+			$("#clearBtn").hide();
+
+			playbackInterruptCommand = "";
+		}, function(){
+			//on playback end
+			$("#playBtn .ui-btn-text").text("Play");
+			$("#playBtn").show();
+			$("#recordBtn").show();
+			$("#clearBtn").show();
+			$("#pauseBtn").hide();
+		}, function() {
+			//on pause
+			$("#pauseBtn .ui-btn-text").text("Resume");
+			$("#recordBtn").hide();
+			$("#playBtn").hide();
+			$("#clearBtn").hide();
+
+		}, function() {
+			//status callback
+			return playbackInterruptCommand;
+		});
+	}
+
+	function pausePlayback()
+	{
+		playbackInterruptCommand = "pause";
+	}
+
+	function resumePlayback()
+	{
+		playbackInterruptCommand = "";
+		drawing.resumePlayback(function(){
+			$("#pauseBtn .ui-btn-text").text("Pause");
+			$("#pauseBtn").show();
+			$("#recordBtn").hide();
+			$("#clearBtn").hide();
+			$("#playBtn").show();
+		});
+	}
