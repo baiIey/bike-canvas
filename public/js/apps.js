8ee5f78edf65c38e11d308d614f01cac2a20c265
Brian Bailey <brbailey@google.com>
2016-11-19 01:35:39 -0800

    remove jquery-mobile css

---
+++ public/js/apps.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD0DnENJ0BWibQdwA1Nw-pnSK_MJBj8tyM",
+  authDomain: "gray-area-mt-canvas.firebaseapp.com",
+  databaseURL: "https://gray-area-mt-canvas.firebaseio.com",
+  storageBucket: "gray-area-mt-canvas.appspot.com",
+  messagingSenderId: "668088233358"
+};
+firebase.initializeApp(config);
